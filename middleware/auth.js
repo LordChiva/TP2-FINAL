d67db0615b526e93771cfda6390ed134d0b87cc2
LordChiva <mjamschon@gmail.com>
2021-06-09 17:33:59 -0300

    changes

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,14 +1,13 @@
 const jwt = require('jsonwebtoken');
 
 function auth(req, res, next){
-try{
-   const token = req.header('Authorization').replace('Bearer ', '');
-   
-   const user = jwt.verify(token, 'ultrasecreta');   
-   next();
-} catch (error){
-    res.status(401).send({error: error.message});
-
-}
+    try{
+    const token = req.header('Token');
+    
+    const user = jwt.verify(token, 'ultrasecreta');   
+    next();
+    } catch (error){
+        res.status(401).send({error: error.message});
+    }
 }
 module.exports = auth;
