dd8b91eea524679fc0a69ecd969bc328ffd30aad
AP <perezariel.net@gmail.com>
2021-06-10 12:09:45 -0300

    limpieza de codigo

--- routes/empleados.js
+++ routes/empleados.js
@@ -36,24 +36,6 @@ router.post('/', async (req, res) => {
     }
 });
 
-// -------- POST CON VALIDACION -------------------------
-// router.post('/', async (req, res) => {
-//     const schema = joi.object({
-//         first: joi.string().alphanum().min(3).required(),
-//         last: joi.string().alphanum().min(3).required(),
-//         year: joi.number().min(1900).max(2020).required()
-//     });
-//     const result = schema.validate(req.body);
-//     console.log(result);
-//     if (result.error) {
-//         res.status(400).send(result.error.details[0].message);
-//     } else {
-//         let inventor = req.body;
-//         inventor = await dataInventor.addInventor(inventor);
-//         res.json(inventor);
-//     }
-// });
-
 router.put('/:id', async (req, res) => {
     const schema = joi.object({
         legajo: joi.number().min(1000).max(9999).required(),
@@ -70,25 +52,6 @@ router.put('/:id', async (req, res) => {
         res.json(empleado);
     }
 });
-
-//---------- PUT CON VALIDACION --------------------
-// router.put('/:id', async (req, res) => {
-//     const schema = joi.object({
-//         first: joi.string().alphanum().min(3),
-//         last: joi.string().alphanum().min(3),
-//         year: joi.number().min(1400).max(2020)
-//     });
-//     const result = schema.validate(req.body);
-//     if (result.error) {
-//         res.status(400).send(result.error.details[0].message);
-//     } else {
-//         let inventor = req.body;
-//         inventor._id = req.params.id;
-//         dataInventor.updateInventor(inventor);
-//         res.json(inventor);
-//     }
-// });
-
 
 router.delete('/:id', async (req, res) => {
     const empleado = await dataEmpleado.getEmpleado(req.params.id)
@@ -108,8 +71,6 @@ router.post('/login', async (req, res) => {
     } catch (error) {
         res.status(401).send(error.message);
     }
-
 });
-
 
 module.exports = router;
